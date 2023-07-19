fd970d37aa197c341c00f0916a3594fa9844e39c
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-19 15:28:07 +0800

    add CatItem component

---
+++ src/components/cats/CatItem.tsx
@@ -0,0 +1,15 @@
+export default function CatItem(props: {
+  name: string;
+  pic: string;
+  desc: string;
+}) {
+  return (
+    <div class='flex flex-col justify-center items-center'>
+      <p>{props.name}</p>
+      <div class='relative group mt-2'>
+        <img src={props.pic} class='hover:opacity-20' />
+        <p class='picDes group-hover:scale-100 '>{props.desc}</p>
+      </div>
+    </div>
+  );
+}
