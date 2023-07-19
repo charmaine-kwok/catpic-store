9978d9865e880c75a07a6f13fa825f351107eed5
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-19 15:29:49 +0800

    import CatItem component

--- src/pages/Cats.tsx
+++ src/pages/Cats.tsx
@@ -2,26 +2,23 @@ import straight from '../assets/catpics/straight.jpg';
 import cook from '../assets/catpics/cook.jpg';
 import ball from '../assets/catpics/ball.jpg';
 import july from '../assets/catpics/july.jpg';
+import CatItem from '../components/cats/catItem';
 
 export default function Cats() {
   return (
     <div class='grid grid-cols-2 md:grid-cols-4 gap-4 justify-between px-2 my-4'>
-      <div class='flex flex-col justify-center items-center'>
-        <p>Straight</p>
-        <img src={straight} />
-      </div>
-      <div class='flex flex-col justify-center items-center'>
-        <p>Cook</p>
-        <img src={cook} />
-      </div>
-      <div class='flex flex-col justify-center items-center'>
-        <p>Ball</p>
-        <img src={ball} />
-      </div>
-      <div class='flex flex-col justify-center items-center'>
-        <p>July</p>
-        <img src={july} />
-      </div>
+      <CatItem
+        name='Straight'
+        pic={straight}
+        desc={'This is Straight! \n She is a good meow!'}
+      />
+      <CatItem
+        name='Cook'
+        pic={cook}
+        desc={'This is Cook! \n She is a good meow!'}
+      />
+      <CatItem name='Ball' pic={ball} desc={'This is Ball.'} />
+      <CatItem name='July' pic={july} desc={'This is July.'} />
     </div>
   );
 }
