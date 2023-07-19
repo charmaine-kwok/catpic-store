38808d209543960bd6549b9309c681a1056a60c0
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-19 16:08:25 +0800

    update hover effect

--- src/components/cats/CatItem.tsx
+++ src/components/cats/CatItem.tsx
@@ -7,8 +7,10 @@ export default function CatItem(props: {
     <div class='flex flex-col justify-center items-center'>
       <p>{props.name}</p>
       <div class='relative group mt-2'>
-        <img src={props.pic} class='hover:opacity-20' />
-        <p class='picDes group-hover:scale-100 '>{props.desc}</p>
+        <img src={props.pic} class='hover:opacity-20 group-hover:opacity-20' />
+        <p class='picDes group-hover:scale-100 group text-black'>
+          {props.desc}
+        </p>
       </div>
     </div>
   );
