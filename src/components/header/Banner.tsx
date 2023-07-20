acb3a6696e4165ea38ebf50a0faeb03baef6935d
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-20 17:55:37 +0800

    update width

--- src/components/header/Banner.tsx
+++ src/components/header/Banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <div class='bg-green-600 w-[100vw] flex items-center justify-center h-[30px]'>
+    <div class='bg-green-600 w-full flex items-center justify-center h-[30px]'>
       <p class='text-white'>Super deal! Free Shipping on Orders Over $50</p>
     </div>
   );
