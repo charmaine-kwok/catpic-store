d5e87100d6ea5c9fe68c0e0b776dd1888f40c4da
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-20 11:18:54 +0800

    add Banner and NavBar components

---
+++ src/components/header/Banner.tsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+  return (
+    <div class='bg-green-600 w-[100vw] flex items-center justify-center h-[30px]'>
+      <p class='text-white'>Super deal! Free Shipping on Orders Over $50</p>
+    </div>
+  );
+}
---
+++ src/components/header/NavBar.tsx
@@ -0,0 +1,37 @@
+import { A } from '@solidjs/router';
+import { Show } from 'solid-js';
+
+import { Cat, Moon, Sun } from '../../assets/icons/Icons';
+
+export default function NavBar(props: {
+  toggleTheme: () => void;
+  darkTheme: boolean;
+}) {
+  return (
+    <div class='border-y py-2 justify-between w-[100vw] flex items-center'>
+      <div class='pl-4 flex flex-row space-x-4 items-center justify-start w-1/4'>
+        <Cat />
+        <A href='/' class='justify-center flex hover:text-[#acabab]'>
+          Home
+        </A>
+
+        <p>
+          <A href='/cats' class='justify-center flex hover:text-[#acabab]'>
+            Cats
+          </A>
+        </p>
+      </div>
+      <A href='/' class='w-1/2 justify-center flex hover:text-[#acabab]'>
+        <p class='font-bold text-xl'>CatPic.</p>
+      </A>
+      <div class='pr-4 w-1/4 flex flex-row space-x-4 items-center justify-end'>
+        <p>Cart</p>
+        <div onClick={() => props.toggleTheme()} class='hover:cursor-pointer'>
+          <Show when={!props.darkTheme} fallback={<Sun />}>
+            <Moon />
+          </Show>
+        </div>
+      </div>
+    </div>
+  );
+}
