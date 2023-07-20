c293fb563134f0d0d4a0310589f46034a0e37389
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-20 14:26:04 +0800

    add description

--- src/pages/Cats.tsx
+++ src/pages/Cats.tsx
@@ -6,19 +6,24 @@ import CatItem from '../components/cats/CatItem';
 
 export default function Cats() {
   return (
-    <div class='grid grid-cols-2 md:grid-cols-4 gap-4 justify-between px-2 my-4'>
-      <CatItem
-        name='Straight'
-        pic={straight}
-        desc={'This is Straight!\nShe is a good meow!'}
-      />
-      <CatItem
-        name='Cook'
-        pic={cook}
-        desc={'This is Cook! \n She is a good meow!'}
-      />
-      <CatItem name='Ball' pic={ball} desc={'This is Ball.'} />
-      <CatItem name='July' pic={july} desc={'This is July.'} />
-    </div>
+    <>
+      <div class='flex justify-center my-6'>
+        <p>Curious about our fabulous felines? Click a cat to learn more!</p>
+      </div>
+      <div class='grid grid-cols-2 md:grid-cols-4 gap-4 justify-between px-2 my-4'>
+        <CatItem
+          name='Straight'
+          pic={straight}
+          desc={'This is Straight!\nShe is a good meow!'}
+        />
+        <CatItem
+          name='Cook'
+          pic={cook}
+          desc={'This is Cook! \n She is a good meow!'}
+        />
+        <CatItem name='Ball' pic={ball} desc={'This is Ball.'} />
+        <CatItem name='July' pic={july} desc={'This is July.'} />
+      </div>
+    </>
   );
 }
