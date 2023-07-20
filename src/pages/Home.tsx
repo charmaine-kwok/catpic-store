c50db60560dd8e35d81f0554fc463f0547678df8
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-20 11:22:34 +0800

    update Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,31 @@
+import straight from '../assets/catpics/straight.jpg';
+import cook from '../assets/catpics/cook.jpg';
+import ball from '../assets/catpics/ball.jpg';
+import july from '../assets/catpics/july.jpg';
+import ImageSlider from '../components/ImageSlider';
+
 export default function Home() {
+  const slides: {
+    pic: string;
+    name: string;
+  }[] = [
+    { pic: straight, name: 'Straight' },
+    { pic: cook, name: 'Cook' },
+    { pic: ball, name: 'Ball' },
+    { pic: july, name: 'July' },
+  ];
   return (
-    <div class='flex justify-center'>
-      <p>hi</p>
-    </div>
+    <>
+      <div class='flex justify-center my-4'>
+        <p class='whitespace-pre-wrap mx-4'>
+          Welcome to the delightful world of four charming felines - Straight,
+          Cook, Ball, and July!\nThis page is dedicated to celebrating the
+          unique personalities and lovable quirks of these four fabulous
+          cats.\nJoin us as we explore their adventures and share their adorable
+          moments with the world.
+        </p>
+      </div>
+      <ImageSlider slides={slides} />
+    </>
   );
 }
