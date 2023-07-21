8bb371ca4c5d59a0e1eb262fe8a2619561853432
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-21 22:20:00 +0800

    update animation

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,56 +1,71 @@
 import { createSignal } from 'solid-js';
-import Dot from './Dot';
 
 export default function ImageSlider(props: { slides: any[] }) {
   const [activeSlide, setActiveSlide] = createSignal(0);
-  function goToSlide(id: number) {
-    setActiveSlide(id);
-  }
+
   function goToNext() {
     setActiveSlide((prev) => (prev === props.slides.length - 1 ? 0 : prev + 1));
+    // setActiveSlide((prev) => prev + 1);
   }
   function goToPrev() {
     setActiveSlide((prev) => (prev === 0 ? props.slides.length - 1 : prev - 1));
+    // setActiveSlide((prev) => prev - 1);
   }
 
-  setInterval(() => {
+  let timer = setInterval(() => {
     goToNext();
   }, 4000);
 
   return (
-    <div class='imageSlider flex-col h-full justify-start'>
-      <p class='font-bold text-2xl'>{props.slides[activeSlide()].name}</p>
-      <div class='imageSlider'>
-        <div class='arrow'>
-          <div onClick={() => goToPrev()} class='cursor-pointer'>
-            ❰
+    <div class='imageSlider flex-col h-full w-full justify-start'>
+      <div class='overflow-hidden relative imageSlider max-h-[380px] h-[80vh] max-w-[500px] w-[80vw]'>
+        <div
+          class={`relative flex translate-x-[${
+            -100 * activeSlide()
+          }%] transition-all duration-700`}
+        >
+          {props.slides.map((item) => (
+            <img
+              src={item.pic}
+              class='min-w-full min-h-full rounded-3xl object-cover'
+            />
+          ))}
+        </div>
+
+        <div class='absolute flex justify-between items-center w-full h-full text-white'>
+          <div>
+            <div class='arrow text-6xl bg-[rgb(0,0,0,0.3)] p-1'>
+              <div
+                onClick={() => {
+                  goToPrev();
+                  clearInterval(timer);
+                  setInterval(() => {
+                    goToNext();
+                  }, 4000);
+                }}
+                class='cursor-pointer'
+              >
+                ❰
+              </div>
+            </div>
+          </div>
+          <div class=''>
+            <div class='arrow text-6xl bg-[rgb(0,0,0,0.3)] p-1'>
+              <div
+                onClick={() => {
+                  goToNext();
+                  clearInterval(timer);
+                  setInterval(() => {
+                    goToNext();
+                  }, 4000);
+                }}
+                class='cursor-pointer'
+              >
+                ❱
+              </div>
+            </div>
           </div>
         </div>
-        <div class='flex items-center justify-center max-h-[380px] max-w-[500px] w-[80vw]'>
-          <img
-            src={props.slides[activeSlide()].pic}
-            class='w-full h-full rounded-3xl'
-          />
-        </div>
-        <div class='arrow'>
-          <div
-            onClick={() => {
-              goToNext();
-            }}
-            class='cursor-pointer'
-          >
-            ❱
-          </div>
-        </div>
-      </div>
-      <div class='flex flex-row space-x-2 justify-center text-[#918d8d]'>
-        {props.slides.map((_, index) => (
-          <Dot
-            index={index}
-            activeSlide={activeSlide()}
-            goToSlide={goToSlide}
-          />
-        ))}
       </div>
     </div>
   );
