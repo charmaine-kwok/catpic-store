fbe51241614e380c7e73b0225da2d0c6c7b35532
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-22 23:44:54 +0800

    add routes

--- src/components/Routers.tsx
+++ src/components/Routers.tsx
@@ -4,13 +4,19 @@ import { lazy } from 'solid-js';
 import Home from '../pages/Home';
 
 const Cats = lazy(() => import('../pages/Cats'));
-const Details = lazy(() => import('../pages/Details'));
+const Straight = lazy(() => import('../pages/Straight'));
+const Cook = lazy(() => import('../pages/Cook'));
+const Ball = lazy(() => import('../pages/Ball'));
+const July = lazy(() => import('../pages/July'));
 
 export default function Routers() {
   return (
     <Routes>
       <Route path='/' component={Home} />
-      <Route path='/cats/:cat' component={Details} />
+      <Route path='/cats/Straight' component={Straight} />
+      <Route path='/cats/Cook' component={Cook} />
+      <Route path='/cats/Ball' component={Ball} />
+      <Route path='/cats/July' component={July} />
       <Route path='/cats' component={Cats} />
     </Routes>
   );
