50a4409cf27a3b6e0c012ba0d08e37297052b66a
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-24 10:15:37 +0800

    fix setInterval bugs

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -15,37 +15,39 @@ export default function ImageSlider(props: { slides: any[] }) {
     setDirection(false);
   }
 
-  let timer = setInterval(() => {
-    goToNext();
-  }, 4000);
+  let timer = setInterval(goToNext, 4000);
 
   return (
     <div class='imageSlider flex-col h-full w-full justify-start'>
       <div class='overflow-hidden relative imageSlider max-h-[380px] h-[80vh] max-w-[500px] w-[80vw]'>
         <div
           onTransitionEnd={() => {
-            direction()
-              ? document
-                  .querySelector('.slider')!
-                  .appendChild(
-                    document.querySelector('.slider')!.firstElementChild
-                  )
-              : document
-                  .querySelector('.slider')!
-                  .prepend(document.querySelector('.slider')!.lastElementChild);
+            if (direction()) {
+              document
+                .querySelector('.slider')!
+                .appendChild(
+                  document.querySelector('.slider')!.firstElementChild
+                );
+            } else {
+              document
+                .querySelector('.slider')!
+                .prepend(document.querySelector('.slider')!.lastElementChild);
+            }
             setIsTransition(false);
             setIsTranslate(false);
             setTimeout(() => setIsTransition(true));
+            clearInterval(timer);
+            setTimeout(() => {
+              timer = setInterval(goToNext, 4000);
+            }, 4000);
           }}
-          class={`relative justify-start slider flex
+          class={`relative justify-start slider flex duration-1000
           -translate-x-[${
             100 +
             (isTranslate() && direction() ? 100 : 0) -
             (isTranslate() && !direction() ? 100 : 0)
           }%]
-          ${
-            isTransition() ? 'transition-all' : 'transition-none'
-          } duration-1000`}
+          ${isTransition() ? 'transition-all' : 'transition-none'} `}
         >
           {props.slides.map((item) => (
             <img
@@ -54,34 +56,26 @@ export default function ImageSlider(props: { slides: any[] }) {
             />
           ))}
         </div>
-
         <div class='absolute flex justify-between items-center w-full h-full text-white'>
-          <div>
-            <div class='arrow text-6xl bg-[rgb(0,0,0,0.3)] p-1'>
-              <div
-                onClick={() => {
-                  goToPrev();
-                  clearInterval(timer);
-                  setInterval(() => goToNext(), 4000);
-                }}
-                class='cursor-pointer'
-              >
-                ❰
-              </div>
+          <div class='arrow text-6xl bg-[rgb(0,0,0,0.3)] p-1'>
+            <div
+              onClick={() => {
+                goToPrev();
+              }}
+              class='cursor-pointer'
+            >
+              ❰
             </div>
           </div>
-          <div class=''>
-            <div class='arrow text-6xl bg-[rgb(0,0,0,0.3)] p-1'>
-              <div
-                onClick={() => {
-                  goToNext();
-                  clearInterval(timer);
-                  setInterval(() => goToNext(), 4000);
-                }}
-                class='cursor-pointer'
-              >
-                ❱
-              </div>
+
+          <div class='arrow text-6xl bg-[rgb(0,0,0,0.3)] p-1'>
+            <div
+              onClick={() => {
+                goToNext();
+              }}
+              class='cursor-pointer'
+            >
+              ❱
             </div>
           </div>
         </div>
