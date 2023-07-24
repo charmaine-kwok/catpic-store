3e15bfdb0d082c88224c2e62ebadd5cdae0fd096
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-24 15:48:02 +0800

    add Support Cats hoverable dropdown menu

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -8,7 +8,8 @@ export default function NavBar(props: {
   toggleTheme: () => void;
   darkTheme: boolean;
 }) {
-  const [isDropdownVisible, setDropdownVisible] = createSignal(false);
+  const [isCatsDropdownVisible, setCatsDropdownVisible] = createSignal(false);
+  const [isSupDropdownVisible, setSupDropdownVisible] = createSignal(false);
   return (
     <div class='border-y py-2 justify-between w-full h-[45px] flex items-center'>
       <div class='pl-4 flex flex-row space-x-4 items-center'>
@@ -19,8 +20,8 @@ export default function NavBar(props: {
         <div class='flex flex-row space-x-8'>
           <div
             class='relative'
-            onMouseEnter={() => setDropdownVisible(true)}
-            onMouseLeave={() => setDropdownVisible(false)}
+            onMouseEnter={() => setCatsDropdownVisible(true)}
+            onMouseLeave={() => setCatsDropdownVisible(false)}
           >
             <A
               href='/cats'
@@ -30,7 +31,7 @@ export default function NavBar(props: {
               <DownArrow />
             </A>
 
-            {isDropdownVisible() && (
+            {isCatsDropdownVisible() && (
               <div class='absolute w-[100px] top-full shadow-xl flex flex-col text-black bg-[#f1f1f1] z-[10]'>
                 <A class='dropdown-content' href='/cats/Straight'>
                   Straight
@@ -53,6 +54,36 @@ export default function NavBar(props: {
           >
             About Cats
           </A>
+          <div
+            class='relative'
+            onMouseEnter={() => setSupDropdownVisible(true)}
+            onMouseLeave={() => setSupDropdownVisible(false)}
+          >
+            <A
+              href='/cats'
+              class=' hover:text-[#acabab] flex flex-row items-center space-x-1'
+            >
+              <p> Support Cats</p>
+              <DownArrow />
+            </A>
+
+            {isSupDropdownVisible() && (
+              <div class='absolute w-[100px] top-full shadow-xl flex flex-col text-black bg-[#f1f1f1] z-[10]'>
+                <A class='dropdown-content' href='https://www.spca.org.hk'>
+                  SPCA
+                </A>
+                <A
+                  class='dropdown-content'
+                  href='https://www.facebook.com/housejoymercy/?locale=zh_HK'
+                >
+                  阿棍屋
+                </A>
+                <A class='dropdown-content' href='https://www.petwithyouhk.com'>
+                  連寵拯救隊
+                </A>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
