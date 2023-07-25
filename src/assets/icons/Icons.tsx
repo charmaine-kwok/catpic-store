d758238875262bde76d7607a70c979b7089b0846
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-25 22:19:02 +0800

    add props 'size' to UpArrow icon

--- src/assets/icons/Icons.tsx
+++ src/assets/icons/Icons.tsx
@@ -157,15 +157,15 @@ export function DownArrow() {
   );
 }
 
-export function UpArrow() {
+export function UpArrow(props: { size: number }) {
   return (
     <svg
       fill='currentColor'
       stroke-width='0'
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 448 512'
-      height='35px'
-      width='35px'
+      height={props.size}
+      width={props.size}
       style='overflow: visible;'
     >
       <path d='M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z'></path>
