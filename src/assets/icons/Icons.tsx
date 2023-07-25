6ee480af5d1871e8263e61caa702d64a45998190
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-25 17:21:58 +0800

    add Close icon

--- src/assets/icons/Icons.tsx
+++ src/assets/icons/Icons.tsx
@@ -176,7 +176,7 @@ export function UpArrow() {
 export function Menu() {
   return (
     <svg
-      fill='#000000'
+      fill='currentColor'
       stroke-width='2'
       xmlns='http://www.w3.org/2000/svg'
       stroke='currentColor'
@@ -188,6 +188,22 @@ export function Menu() {
       style='overflow: visible;'
     >
       <path d='M3 12h18M3 6h18M3 18h18'></path>
+    </svg>
+  );
+}
+
+export function Close() {
+  return (
+    <svg
+      fill='currentColor'
+      stroke-width='0'
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 512 512'
+      height='20px'
+      width='20px'
+      style='overflow: visible;'
+    >
+      <path d='M400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49z'></path>
     </svg>
   );
 }
