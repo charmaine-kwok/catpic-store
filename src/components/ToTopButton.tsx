e4cc6e4436429951086f24915c30459b8ee04838
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-25 22:20:42 +0800

    update ToTopButton position and pass props 'size' to UpArrow icon

--- src/components/ToTopButton.tsx
+++ src/components/ToTopButton.tsx
@@ -4,12 +4,12 @@ import toTop from './functions/toTop';
 export default function ToTopButton(props: { darkTheme: boolean }) {
   return (
     <div
-      class={`flex items-center justify-center fixed bottom-10 right-5 w-[45px] h-[45px] opacity-60 ${
+      class={`flex items-center justify-center fixed bottom-20 right-5 w-[45px] h-[45px] opacity-60 ${
         props.darkTheme ? 'bg-[#ffffff] text-black' : 'bg-[#000000] text-white'
       } rounded-md cursor-pointer hover:opacity-100`}
       onClick={toTop}
     >
-      {<UpArrow />}
+      {<UpArrow size={35} />}
     </div>
   );
 }
