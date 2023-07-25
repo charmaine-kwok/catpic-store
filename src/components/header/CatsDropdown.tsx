6dbd0485cb32bdc08b582187b9ecaba0c51a51cd
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-25 17:20:01 +0800

    seperate CatsDropdown component into CatsDropdown and CatsDropdownContent

--- src/components/header/CatsDropdown.tsx
+++ src/components/header/CatsDropdown.tsx
@@ -3,6 +3,14 @@ import { A } from '@solidjs/router';
 export default function CatsDropdown() {
   return (
     <div class='absolute w-[100px] top-full shadow-xl flex flex-col text-black bg-[#f1f1f1] z-[10]'>
+      <CatsDropdownContent />
+    </div>
+  );
+}
+
+export function CatsDropdownContent() {
+  return (
+    <>
       <A class='dropdown-content' href='/cats'>
         Our Cats
       </A>
@@ -21,6 +29,6 @@ export default function CatsDropdown() {
       <A class='dropdown-content' href='https://en.wikipedia.org/wiki/Cat'>
         About Cats
       </A>
-    </div>
+    </>
   );
 }
