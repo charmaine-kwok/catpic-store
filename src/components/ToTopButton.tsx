c872db64a7075898a28a0d6b2880042112d91b0a
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-25 10:40:09 +0800

    import toTop function

--- src/components/ToTopButton.tsx
+++ src/components/ToTopButton.tsx
@@ -1,4 +1,5 @@
 import { UpArrow } from '../assets/icons/Icons';
+import toTop from './functions/toTop';
 
 export default function ToTopButton(props: { darkTheme: boolean }) {
   return (
@@ -6,7 +7,7 @@ export default function ToTopButton(props: { darkTheme: boolean }) {
       class={`flex items-center justify-center fixed bottom-10 right-5 w-[45px] h-[45px] opacity-60 ${
         props.darkTheme ? 'bg-[#ffffff] text-black' : 'bg-[#000000] text-white'
       } rounded-md cursor-pointer hover:opacity-100`}
-      onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+      onClick={toTop}
     >
       {<UpArrow />}
     </div>
