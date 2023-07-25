17cd342cbe04d2383a87cd96bc4280448981e44f
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-25 10:43:18 +0800

    add 'Our Cats' link in CatsDropdown component

--- src/components/header/CatsDropdown.tsx
+++ src/components/header/CatsDropdown.tsx
@@ -3,6 +3,9 @@ import { A } from '@solidjs/router';
 export default function CatsDropdown() {
   return (
     <div class='absolute w-[100px] top-full shadow-xl flex flex-col text-black bg-[#f1f1f1] z-[10]'>
+      <A class='dropdown-content' href='/cats'>
+        Our Cats
+      </A>
       <A class='dropdown-content' href='/cats/Straight'>
         Straight
       </A>
