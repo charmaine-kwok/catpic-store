fb94e1aa98d770e425221e2ac8011ccad662e8e5
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-26 13:50:22 +0800

    update CatDetails component with responsive image sizing and alt text

--- src/components/cats/CatDetails.tsx
+++ src/components/cats/CatDetails.tsx
@@ -4,11 +4,16 @@ export default function CatDetails(props: {
   pic: string;
   heading: string;
   desc: JSX.Element;
+  catName: string;
 }) {
   return (
-    <div class='grid grid-cols-1 md:grid-cols-2 justify-center items-center h-full'>
+    <div class='grid grid-cols-1 md:grid-cols-2 justify-center items-center h-[100%]'>
       <div class='p-8'>
-        <img src={props.pic} />
+        <img
+          src={props.pic}
+          class='md:h-auto h-[30vh] md:w-auto w-[100%]'
+          alt={`Picture of ${props.catName}`}
+        />
       </div>
       <div class='p-8 h-full flex flex-col justify-center'>
         <p class='font-bold mb-2'>{props.heading}</p>
