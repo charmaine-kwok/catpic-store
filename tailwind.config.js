a2301313956752da1bd9cc1f063a04da5184ba7f
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-27 12:05:20 +0800

    add customized breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
-  },
+    screens: {
+      'mobile': '480px',
+      ...defaultTheme.screens,
+    },
+    },
   plugins: [],
 };
