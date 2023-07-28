c830cda4972214decd3e87488125ada7d70af9c8
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-28 15:05:38 +0800

    update import pic dir and import small pics

--- src/pages/Cats.tsx
+++ src/pages/Cats.tsx
@@ -1,7 +1,11 @@
-import straight from '../assets/catpics/straight.jpg';
-import cook from '../assets/catpics/cook.jpg';
-import ball from '../assets/catpics/ball.jpg';
-import july from '../assets/catpics/july.jpg';
+import straight from '../assets/catpics/actual/straight.jpg';
+import smallStraight from '../assets/catpics/small/straight-small.jpg';
+import cook from '../assets/catpics/actual/cook.jpg';
+import smallCook from '../assets/catpics/small/cook-small.jpg';
+import ball from '../assets/catpics/actual/ball.jpg';
+import smallBall from '../assets/catpics/small/ball-small.jpg';
+import july from '../assets/catpics/actual/july.jpg';
+import smallJuly from '../assets/catpics/small/july-small.jpg';
 import CatItem from '../components/cats/CatItem';
 
 export default function Cats() {
@@ -10,20 +14,31 @@ export default function Cats() {
       <div class='flex text-center my-6 px-4'>
         <p>Curious about our fabulous felines? Click a cat to learn more!</p>
       </div>
-
       <div class='grid grid-cols-2 lg:grid-cols-4 gap-4 w-[90%]'>
         <CatItem
           name='Straight'
-          pic={straight}
+          actualPic={straight}
+          smallPic={smallStraight}
           desc={'This is Straight!\nShe is a good meow!'}
         />
         <CatItem
           name='Cook'
-          pic={cook}
+          actualPic={cook}
+          smallPic={smallCook}
           desc={'This is Cook! \n She is a good meow!'}
         />
-        <CatItem name='Ball' pic={ball} desc={'This is Ball.'} />
-        <CatItem name='July' pic={july} desc={'This is July.'} />
+        <CatItem
+          name='Ball'
+          actualPic={ball}
+          smallPic={smallBall}
+          desc={'This is Ball.'}
+        />
+        <CatItem
+          name='July'
+          actualPic={july}
+          smallPic={smallJuly}
+          desc={'This is July.'}
+        />
       </div>
     </div>
   );
