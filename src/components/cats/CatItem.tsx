b27adba7824bc18a63b47ca1b609ee341ce77553
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-28 15:04:53 +0800

    import LazyImage

--- src/components/cats/CatItem.tsx
+++ src/components/cats/CatItem.tsx
@@ -1,18 +1,20 @@
 import { A } from '@solidjs/router';
+import LazyImage from '../LazyImage';
 
 export default function CatItem(props: {
   name: string;
-  pic: string;
   desc: string;
+  smallPic: string;
+  actualPic: string;
 }) {
   return (
     <div class='flex flex-col justify-center'>
       <p class='text-center h-[10%]'>{props.name}</p>
       <A href={`/cats/${props.name}`} class='h-[90%] relative group mt-2'>
-        <img
-          src={props.pic}
-          class='w-full h-full hover:opacity-20 group-hover:opacity-20'
-          alt={`Picture of ${props.name}`}
+        <LazyImage
+          smallPic={props.smallPic}
+          actualPic={props.actualPic}
+          name={props.name}
         />
         <p class='picDes group-hover:scale-100 group'>{props.desc}</p>
       </A>
